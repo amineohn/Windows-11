d73cb3762af709e8a88906bdd9ae1026938ab670
amine.im <amineprojet7@gmail.com>
2022-02-05 20:05:40 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,33 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const path = require("path");
+
+const config = (phase) => {
+    let plugins = [];
+    let settings = {
+        reactStrictMode: true,
+        webpack: (config) => {
+            config.resolve.alias = {
+                ...config.resolve.alias,
+                "@": path.join(__dirname, "src"),
+                "@public": path.join(__dirname, "public"),
+            };
+
+            return config;
+        },
+    };
+
+    if (phase !== PHASE_DEVELOPMENT_SERVER) {
+        // do something in production mode
+    }
+
+    return {
+        plugins,
+        settings,
+    };
+};
+
+const pipe = (funcs) => (value) => funcs.reduce((v, f) => f(v), value);
+module.exports = (phase, { defaultConfig }) => {
+    const cfg = config(phase, { defaultConfig });
+    return pipe(cfg.plugins)(cfg.settings);
+};
