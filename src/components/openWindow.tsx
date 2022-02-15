fc3d79f489e55562c628b6e092d242f18af341cc
amineohn <amineprojet7@gmail.com>
2022-02-15 12:52:24 +0100

    fix: openWindoow

--- src/components/openWindow.tsx
+++ src/components/openWindow.tsx
@@ -2,7 +2,7 @@ import { openWindowProps } from "@/interfaces";
 
 const OpenWindow = ({ onClick, icon, title }: openWindowProps) => {
     return (
-        <div onClick={onClick} className="flex justify-center items-center flex-col hover:bg-white/10 rounded-lg transition-colors py-4 px-4 cursor-pointer">
+        <div onClick={onClick} className="flex justify-center items-center flex-col hover:bg-white/10 rounded-lg transition-colors py-4 px-4 cursor-pointer space-y-1.5">
             {icon}
             <span className="text-white/90 text-md font-medium">{title}</span>
         </div>
