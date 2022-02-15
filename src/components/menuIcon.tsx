9c30934f4a2441906e1ad645ff87ab46ad6fb019
isveny <amineprojet7@gmail.com>
2022-02-15 21:09:37 +0100

    support darkmode finished.

--- src/components/menuIcon.tsx
+++ src/components/menuIcon.tsx
@@ -1,7 +1,7 @@
 import { MenuIconProps } from "@/interfaces";
 const MenuIcon = ({ icon, title }: MenuIconProps) => {
     return (
-        <div className="w-[96px] h-[84px] hover:bg-white/10 transition-colors ease-in-out duration-150 rounded-md">
+        <div className="w-[96px] h-[84px] hover:bg-black/5 dark:hover:bg-white/10 transition-colors ease-in-out duration-150 rounded-md">
             <div className="flex justify-center items-center flex-col space-y-2 mt-2">
                 {icon}
 
